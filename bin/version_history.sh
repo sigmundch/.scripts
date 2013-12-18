416fbab8f34fbe13832d62e53cd52224bba02de2
Sigmund Cherem <sigmund@google.com>
2013-12-18 15:48:25 -0800

    version_history.sh some updates

--- bin/version_history.sh
+++ bin/version_history.sh
@@ -3,12 +3,17 @@
 limit=5
 if [[ $# -gt 0 ]]; then
   limit=$1
+  shift
 fi
 
+channel=trunk
+if [[ $# -gt 0 ]]; then
+  channel=$1
+fi
 
-svn log -l $limit https://dart.googlecode.com/svn/trunk dart/tools/VERSION | \
+svn log -l $limit https://dart.googlecode.com/svn/$channel dart/tools/VERSION | \
     grep "r[0-9]\+\|Version\|merge" | \
     sed -e "s/\(r[0-9]\+\) | .*/\n(\1)/" | \
     sed -e "s/Version \([^ ]*\).*/  [32mversion \1:[0m/" | \
-    sed -e "s/svn merge -c \([0-9,]*\) .*/  +\1/" | \
-    sed -e "s/svn merge -r\( \)\?\([0-9]*\):\([0-9]*\) .*/  \2 .. \3/"
+    sed -e "s/svn merge -c \([0-9,]*\) .*/  +\1              \/\/ \0/" | \
+    sed -e "s/svn merge -r\( \)\?\([0-9]*\):\([0-9]*\) .*/  \2 .. \3      \/\/ \0/"
