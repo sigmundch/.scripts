25d01bdd60de90c2abd860e18b43a7d79b0bc6dc
Sigmund Cherem <sigmund@google.com>
2022-10-14 23:06:22 -0400

    show battery/fan/other info in status bar

--- bin/status_line.sh
+++ bin/status_line.sh
@@ -17,4 +17,16 @@ fi
 # we also used to have: `uptime | sed 's/.*,//'`
 keyboard=$(echo $(setxkbmap -query | grep "layout\|variant" | awk '{print toupper($2)}'))
 datetime=$(date +"%T . %D")
-echo "$notification_message [$keyboard] $datetime";
+
+raw_temp=$(cat /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon?/temp1_input)
+fan_speed="$(cat /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon?/fan1_input)\U0001F300"
+temp="$(echo $raw_temp | cut -b -2).$(echo $raw_temp | cut -b 3- | cut -b -1)°C"
+battery="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}')"
+wifi_name="$(nmcli -t -f name c show --active | sed -e 's/Auto //')\U0001F4F6"
+
+if [[ "$(bluetoothctl info 7C:96:D2:6E:F0:28 | grep Connected | sed -e "s/.*Connected: //")" == "yes" ]]; then
+  headset="\U0001F3A7"
+else
+  headset="\U0001F50A"
+fi
+echo -e "$notification_message [$keyboard] $datetime . $temp $fan_speed $battery\U0001F50B $wifi_name $headset";
