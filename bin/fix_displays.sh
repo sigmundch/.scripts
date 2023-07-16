9b2205127c6cdf8328cb09c9d4b90a408f4f6888
Sigmund Cherem <sigmund@google.com>
2023-07-15 19:42:55 -0700

    [bin] extra scripts for fixing middle button, connecting bluetooth, clock, etc

---
+++ bin/fix_displays.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xrandr --output DP-1 --auto --left-of eDP-1
+xrandr --output eDP-1 --scale 0.4x0.4 --dpi 96
---
+++ bin/red_clock.sh
@@ -0,0 +1,1 @@
+xclock -d -render -strftime "%H:%M:%S" -update 1 -face "Monospace-50" -fg "#ff3333" -bg "black" &
